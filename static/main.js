9a277be6eaa28713f4d508bc4841f14afb9b7d3d
Noah Hüsser <yatekii@yatekii.ch>
2020-12-31 03:41:01 +0100

    A little bit of formatting

--- static/main.js
+++ static/main.js
@@ -18,6 +18,7 @@ export function main() {
     m.mount(document.body, {
         view(vnode) {
             return m('.container', [
+                m('row', m('.col', m('h2.text-center.m-4', 'probe-rs performance & regression tracking'))),
                 m('.row', plots.map((p, i) => m('.col-6.plot', {
                     oninit(vnode) {
                         vnode.state.plot = undefined;
@@ -49,6 +50,19 @@ export function main() {
     })
 }
 
+/// https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
+function formatBytes(bytes, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
 function createPlot(plot, dom) {
     return m.request({
         method: 'GET',
@@ -59,7 +73,11 @@ function createPlot(plot, dom) {
             result.logs.map(
                 (v, i) => i),
             result.logs.map(v => v.read_speed),
-            result.logs.map(v => v.write_speed)
+            result.logs.map(v => v.write_speed),
+            result.logs.map(v => v.read_speed / result.logs[0].read_speed * 100 - 100),
+            result.logs.map(v => v.write_speed / result.logs[0].write_speed * 100 - 100),
+            result.logs.map((v, i) => v.read_speed / result.logs[Math.max(i - 1, 0)].read_speed * 100 - 100),
+            result.logs.map((v, i) => v.write_speed / result.logs[Math.max(i - 1, 0)].write_speed * 100 - 100)
         ];
         const opts = {
             width: dom.clientWidth,
@@ -72,8 +90,14 @@ function createPlot(plot, dom) {
                     rotate: -90,
                     size: 100,
                 }, {
-                    values: (self, vals) => vals.map(i => i),
-                    label: 'Transfer Speed [Bytes]'
+                    values: (self, vals) => vals.map(b => formatBytes(b)),
+                    scale: 'b',
+                    size: 70,
+                }, {
+                    values: (self, vals) => vals.map(b => b.toFixed(2) + '%'),
+                    side: 1,
+                    scale: '%',
+                    size: 70,
                 }
             ],
             scales: {
@@ -89,11 +113,37 @@ function createPlot(plot, dom) {
                 }, {
                     stroke: 'orange',
                     label: 'read',
-                    value: (self, i) => i
+                    value: (self, b) => formatBytes(b),
+                    scale: 'b',
                 }, {
                     stroke: 'red',
                     label: 'write',
-                    value: (self, i) => i
+                    value: (self, b) => formatBytes(b),
+                    scale: 'b',
+                }, {
+                    stroke: 'orange',
+                    dash: [10, 5],
+                    label: 'abs Δread',
+                    value: (self, b) => b.toFixed(2) + '%',
+                    scale: '%',
+                }, {
+                    stroke: 'red',
+                    dash: [10, 5],
+                    label: 'abs Δwrite',
+                    value: (self, b) => b.toFixed(2) + '%',
+                    scale: '%',
+                }, {
+                    stroke: 'orange',
+                    dash: [5, 10],
+                    label: 'Δread',
+                    value: (self, b) => b.toFixed(2) + '%',
+                    scale: '%',
+                }, {
+                    stroke: 'red',
+                    dash: [5, 10],
+                    label: 'Δwrite',
+                    value: (self, b) => b.toFixed(2) + '%',
+                    scale: '%',
                 }
             ],
         };
