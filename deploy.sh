0e9e8eac31d6de946363fb9f1f943da664231bc0
Noah Hüsser <yatekii@yatekii.ch>
2021-01-01 01:34:56 +0100

    Fix deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 ssh nebuchadnezar 'git clone https://github.com/probe-rs/perfbot'
 ssh nebuchadnezar 'cd perfbot && git pull'
 ssh nebuchadnezar 'killall perfbot'
-ssh nebuchadnezar 'source $HOME/.cargo/env && cd perfbot && cargo run --release'
+ssh nebuchadnezar 'source $HOME/.cargo/env && cd perfbot && nohup cargo run --release > /dev/null 2>&1 &'
