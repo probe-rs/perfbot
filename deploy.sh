455a64cfba512a66721c0416abd67954055fa8d0
Noah Hüsser <yatekii@yatekii.ch>
2021-01-01 01:16:25 +0100

    Maybe fix port

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 ssh nebuchadnezar 'git clone https://github.com/probe-rs/perfbot'
-ssh nebuchadnezar 'cd perfbot'
-ssh nebuchadnezar 'git pull'
-ssh nebuchadnezar 'cargo run --release'
+ssh nebuchadnezar 'cd perfbot && git pull'
+ssh nebuchadnezar 'killall perfbot'
+ssh nebuchadnezar 'source $HOME/.cargo/env && cd perfbot && cargo run --release'
