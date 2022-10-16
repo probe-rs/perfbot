1c0caf083e664252f1388150c6d81009be5ee728
Dominik Boehi <dominik.boehi@gmail.com>
2022-10-16 12:54:21 +0200

    Update matrix SDK to 0.4.0

--- src/matrix/handlers.rs
+++ src/matrix/handlers.rs
@@ -1,10 +1,14 @@
+use std::sync::Arc;
+
 use askama::Template;
 use diesel::{dsl::sql, sql_types::Text, QueryDsl, RunQueryDsl, TextExpressionMethods};
 use matrix_sdk::{
-    api::r0::message::send_message_event::Response,
-    events::{room::message::MessageEventContent, AnyMessageEventContent},
     room::Joined,
-    Client, Error,
+    ruma::{
+        api::client::r0::message::send_message_event::Response,
+        events::{room::message::MessageEventContent, AnyMessageEventContent},
+    },
+    Error,
 };
 use perfbot_common::{schema::logs, Log};
 
@@ -12,33 +16,28 @@ use crate::get_pr_commits;
 
 use super::templates::{HelpTemplate, PerfTemplate};
 
-pub async fn help(client: Client, room: Joined) -> Result<Response, Error> {
+pub async fn help(room: Joined) -> Result<Response, Error> {
     let content = AnyMessageEventContent::RoomMessage(MessageEventContent::text_html(
         "",
         HelpTemplate {}.render().unwrap(),
     ));
 
-    // TODO: Not sure if necessary anymore, lock seems no longer present on newer matrix-sdk version
-    // we clone here to hold the lock for as little time as possible.
-    let room_id = room.room_id().clone();
-
-    client
+    room
         // send our message to the room we found the "!party" command in
         // the last parameter is an optional Uuid which we don't care about.
-        .room_send(&room_id, content, None)
+        .send(content, None)
         .await
 }
 
 pub async fn perf(
-    private_key: &[u8],
-    database_path: &str,
-    client: Client,
+    private_key: Arc<Vec<u8>>,
+    database_path: Arc<String>,
     room: Joined,
 ) -> Result<Response, Error> {
     // TODO:
     //
     // 1. Get open PRs.
-    let prs = get_pr_commits(private_key).await.unwrap();
+    let prs = get_pr_commits(&private_key).await.unwrap();
     // 2. Get perf benchmarks for last commit of open PRs.
     let prs = prs
         .iter()
@@ -49,7 +48,7 @@ pub async fn perf(
                     sql::<Text>(&format!("'{}'", commits.last().unwrap()))
                         .like(logs::commit_hash.concat("%")),
                 )
-                .load::<Log>(&crate::db::establish_connection(database_path))
+                .load::<Log>(&crate::db::establish_connection(&database_path))
                 .unwrap()
                 .len(),
         })
@@ -62,15 +61,9 @@ pub async fn perf(
         PerfTemplate { prs }.render().unwrap(),
     ));
 
-    // TODO: Not sure if necessary anymore, lock seems no longer present on newer matrix-sdk version
-    // we clone here to hold the lock for as little time as possible.
-    let room_id = room.room_id().clone();
-
-    client
-        // send our message to the room we found the "!party" command in
-        // the last parameter is an optional Uuid which we don't care about.
-        .room_send(&room_id, content, None)
-        .await
+    // send our message to the room we found the "!party" command in
+    // the last parameter is an optional Uuid which we don't care about.
+    room.send(content, None).await
 }
 
 pub struct Pr {
